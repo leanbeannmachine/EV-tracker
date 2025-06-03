0c1e2e0ce13ef18ef6268a9737ced010fc5ea2f2
leanbeannmachine <72333998+leanbeannmachine@users.noreply.github.com>
2025-06-03 17:07:48 -0500

    Update telegram_alert_bot.py
    Had to make sure token was fixed for the last time.

--- telegram_alert_bot.py
+++ telegram_alert_bot.py
@@ -1,8 +1,7 @@
 import os
 import requests
 
-BOT_TOKEN = "7607490683:AAH5LZ3hHnTimx35du-
-UQanEQBXpt6otjcI"
+BOT_TOKEN = "7607490683:AAH5LZ3hHnTimx35du-UQanEQBXpt6otjcI"
 CHAT_ID = "964091254"
 
 def send_telegram_message(bet):
