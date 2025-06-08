d65a324b3749e80a2631ebd9e279ebf24768d61d
leanbeannmachine <72333998+leanbeannmachine@users.noreply.github.com>
2025-06-07 23:40:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def format_message(game, market, outcome, odds, ev, start_time):
     )
 
 def send_telegram_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
     try:
         bot.send_message(chat_id=CHAT_ID, text=message, parse_mode=telegram.ParseMode.MARKDOWN)
     except telegram.error.TelegramError as e:
