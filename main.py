692829de00301e84b24628c258a94e60cc7691a5
leanbeannmachine <72333998+leanbeannmachine@users.noreply.github.com>
2025-06-09 11:17:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import telegram
 import json
 import os
 
-API_KEY = "b478dbe3f62f1f249a7c319cb2248bc5"
+API_KEY = "9007d620a2ee59fb441c45ffdf058ea6"
 TELEGRAM_BOT_TOKEN = "7607490683:AAH5LZ3hHnTimx35du-UQanEQBXpt6otjcI"
 TELEGRAM_CHAT_ID = "964091254"
 
